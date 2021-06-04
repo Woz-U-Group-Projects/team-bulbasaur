3136cf6f414acca54a3aff4e40e2e81f8836a276
Brandon Santana <BrandonS1123581321@gmail.com>
2021-06-04 00:19:09 -0500

    push

--- client-react/src/components/login/login.js
+++ client-react/src/components/login/login.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import { formik } from 'formik'
+import * as yup from 'yup'
+
+const  validate = yup.object({
+  email: yup.string().email('Must Be A Valid Email Address').required('An Email Is Needed To')
+})
 
 const Login = (props) => {
   return (
