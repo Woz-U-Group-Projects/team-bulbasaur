828531ae915487c899ebd3f7bc71759b006c0598
Duy Nguyen <duyl.nguyen.ga@gmail.com>
2021-06-21 02:06:13 -0400

    Finish styling Signup page

--- client-react/src/components/signup/signup.js
+++ client-react/src/components/signup/signup.js
@@ -5,12 +5,18 @@ import * as yup from 'yup';
 
 import './signup.css';
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle, faEnvelope, faUser, faLock } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core'
+
 const validate = yup.object({
   name: yup.string().required('Your Full Name Is Required').min(3, 'Your Name Must Be 4 Characters Long Or More'),
   email: yup.string().email('Invalid Email Address').required('You Need An Email To Sign Up'),
   userName: yup.string().required('You Need To Create A User Name').min(5, 'Your User Name Must Required A Minimum Of 5 Characters'),
   password: yup.string().required().min(8, 'Your Password Must Be A Minimum Of 8 Characters')
 })
+
+library.add(faUserCircle, faEnvelope, faUser, faLock);
 
 const SignUp = ({ onSignup, signupStatus }) => {
   let [signUpSuccessful, setSuccess] = useState(false)
@@ -50,23 +56,29 @@ const SignUp = ({ onSignup, signupStatus }) => {
             id='name'
             name='name'
             type='text'
+            placeholder='Full Name'
             value={formik.values.name}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
           />
-          {formik.touched.name && formik.errors.name ? <div>{formik.errors.name}</div> : null}
+          <FontAwesomeIcon className="icon-user-signup" icon="user-circle" />
+          {/* {formik.touched.name && formik.errors.name ? <div>{formik.errors.name}</div> : null} */}
         </div>
+
         <div className="signup-email">
           {/* <label htmlFor='email'>Email:</label> */}
           <input
             id='email'
             name='email'
             type='email'
+            placeholder='Email'
             value={formik.values.email}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
           />
-          {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
+          <FontAwesomeIcon className="icon-user-signup" icon="envelope" />
+          {/* {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null} */}
+    
         </div>
         <div className="signup-username">
           {/* <label htmlFor='userName'>User Name:</label> */}
@@ -74,26 +86,32 @@ const SignUp = ({ onSignup, signupStatus }) => {
             id='userName'
             name='userName'
             type='text'
+            placeholder='User Name'
             value={formik.values.userName}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
           />
-          {formik.touched.userName && formik.errors.userName ? <div>{formik.errors.userName}</div> : null}
+          <FontAwesomeIcon className="icon-user-signup" icon="user" />
+          {/* {formik.touched.userName && formik.errors.userName ? <div>{formik.errors.userName}</div> : null} */}
         </div>
+
         <div className="signup-password">
           {/* <label htmlFor='password'>Password:</label> */}
           <input
             id='password'
             name='password'
             type='password'
+            placeholder='Password'
             value={formik.values.password}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
           />
-          {formik.touched.password && formik.errors.password ? <div>{formik.errors.password}</div> : null}
+          <FontAwesomeIcon className="icon-user-signup" icon="lock" />
+          {/* {formik.touched.password && formik.errors.password ? <div>{formik.errors.password}</div> : null} */}
         </div>
+
         <div className="signup-btn">
-          <button type='submit'>Submit</button>
+          <button type='submit'>CREATE ACCOUNT</button>
         </div>
       </form>
     </div>
