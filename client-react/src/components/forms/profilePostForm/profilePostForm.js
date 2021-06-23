d967223a4dfd2d9f32d27fce5a0786493362c69b
Duy Nguyen <duyl.nguyen.ga@gmail.com>
2021-06-23 00:25:22 -0400

    Styling profile page

--- client-react/src/components/forms/profilePostForm/profilePostForm.js
+++ client-react/src/components/forms/profilePostForm/profilePostForm.js
@@ -25,44 +25,86 @@ const ProfilePostForm = (props) => {
   })
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <div>
-        <label htmlFor='title'>Title</label>
-        <input
-          id='title'
-          name='title'
-          type='text'
-          value={formik.values.title}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.title && formik.errors.title ? <div>{formik.errors.title}</div> : null}
-      </div>
-      <div>
-        <label htmlFor='body'>Post</label>
-        <input
-          id='body'
-          name='body'
-          type='text'
-          value={formik.values.body}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.body && formik.errors.body ? <div>{formik.errors.body}</div> : null}
-      </div>
-      <div>
-        <label htmlFor='isHidden'>Private</label>
-        <input
-          id='isHidden'
-          name='isHidden'
-          type='checkbox'
-          onChange={formik.handleChange}
-        />
-      </div>
-      <div>
-        <button type='submit' >Submit</button>
-      </div>
-    </form>
+    // <form onSubmit={formik.handleSubmit}>
+    //   <div>
+    //     <label htmlFor='title'>Title</label>
+    //     <input
+    //       id='title'
+    //       name='title'
+    //       type='text'
+    //       value={formik.values.title}
+    //       onChange={formik.handleChange}
+    //       onBlur={formik.handleBlur}
+    //     />
+    //     {formik.touched.title && formik.errors.title ? <div>{formik.errors.title}</div> : null}
+    //   </div>
+    //   <div>
+    //     <label htmlFor='body'>Post</label>
+    //     <input
+    //       id='body'
+    //       name='body'
+    //       type='text'
+    //       value={formik.values.body}
+    //       onChange={formik.handleChange}
+    //       onBlur={formik.handleBlur}
+    //     />
+    //     {formik.touched.body && formik.errors.body ? <div>{formik.errors.body}</div> : null}
+    //   </div>
+    //   <div>
+    //     <label htmlFor='isHidden'>Private</label>
+    //     <input
+    //       id='isHidden'
+    //       name='isHidden'
+    //       type='checkbox'
+    //       onChange={formik.handleChange}
+    //     />
+    //   </div>
+    //   <div>
+    //     <button type='submit' >Submit</button>
+    //   </div>
+    // </form>
+    <form className='form-container' onSubmit={formik.handleSubmit}>
+        <div className='form-title'>
+          <label htmlFor='title'>Title</label>
+          <input
+            id='title'
+            name='title'
+            type='text'
+            placeholder="...Your post's title here..."
+            value={formik.values.title}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.title && formik.errors.title ? <div>{formik.errors.title}</div> : null}
+        </div>
+        <div className='form-post-body'>
+          <label htmlFor='body'></label>
+          <textarea
+            id='body'
+            name='body'
+            type='text'
+            value={formik.values.body}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.body && formik.errors.body ? <div>{formik.errors.body}</div> : null}
+        </div>
+        <div className='form-btn-wrapper'>
+          <div className='private-option'>
+            <label htmlFor='isHidden'>Private</label>
+            <input
+              id='isHidden'
+              name='isHidden'
+              type='checkbox'
+              onChange={formik.handleChange}
+            />
+          </div>
+          <div className='form-submit-btn'>
+            <button className='form-submit-btn-action' type='submit'>Submit</button>
+          </div>
+        </div>
+      </form>
+
   )
 } 
 
